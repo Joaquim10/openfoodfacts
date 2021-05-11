a0d8ce8d076eaa9f51fc6dd753d1b0b5c68cbf87
Joaquim10 <jodossantos@free.fr>
2021-05-11 21:25:09 +0200

    Add documentation

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
+"""settings contains the settings of the application"""
 
+# Product categories
+# You can change, add and delete the product categories being used here.
+# The valid categories can be found on this page:
+# https://fr.openfoodfacts.org/categories.json
+# On this page, you can filter the categories by name and develop all.
+# This operation is slow.
+#
 CATEGORIES = [
     'Légumes et dérivés',
     'Plats à base de riz',
